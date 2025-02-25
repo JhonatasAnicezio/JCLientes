86dc7b7801b22efa0428523558949e29ee58fd87
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-02-25 07:24:04 -0300

    feat: create method create in controller for route POST /clientes

--- src/main/java/com/api/clientes/controller/PersonController.java
+++ src/main/java/com/api/clientes/controller/PersonController.java
@@ -1,8 +1,12 @@
 package com.api.clientes.controller;
 
+import com.api.clientes.controller.dto.PersonCreationDto;
+import com.api.clientes.controller.dto.PersonDto;
 import com.api.clientes.service.PersonService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
@@ -11,5 +15,12 @@ public class PersonController {
 
   public PersonController(PersonService personService) {
     this.personService = personService;
+  }
+
+  @PostMapping
+  public ResponseEntity<PersonDto> create(
+      @Valid @RequestBody PersonCreationDto personCreationDto) {
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(PersonDto.fromEntity(personService.create(personCreationDto.toEntity())));
   }
 }

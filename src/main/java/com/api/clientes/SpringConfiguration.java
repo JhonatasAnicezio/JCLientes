6cf99e8354c2f93a987869e78ee6d34eaca5450b
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-03-05 20:44:37 -0300

    feat: add new route for all

--- src/main/java/com/api/clientes/SpringConfiguration.java
+++ src/main/java/com/api/clientes/SpringConfiguration.java
@@ -25,6 +25,7 @@ public class SpringConfiguration {
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST, "/clients").permitAll()
             .requestMatchers(HttpMethod.POST, "/authentication").permitAll()
+            .requestMatchers(HttpMethod.GET, "/clients/me").permitAll()
             .anyRequest().authenticated())
         .build();
   }

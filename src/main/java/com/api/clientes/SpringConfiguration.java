7ee6098abdb076f1cf9f8f3c816878e57eb6ed3b
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-03-05 20:42:28 -0300

    feat: implments private routes

--- src/main/java/com/api/clientes/SpringConfiguration.java
+++ src/main/java/com/api/clientes/SpringConfiguration.java
@@ -2,6 +2,7 @@ package com.api.clientes;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,6 +22,10 @@ public class SpringConfiguration {
     return httpSecurity
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/clients").permitAll()
+            .requestMatchers(HttpMethod.POST, "/authentication").permitAll()
+            .anyRequest().authenticated())
         .build();
   }
 
